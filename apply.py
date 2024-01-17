095d628c9e36098e0e282f02636663ebec1193d6
heyscottn <heyscottn@gmail.com>
2024-01-17 12:34:32 -0600

    edits

--- apply.py
+++ apply.py
@@ -1,11 +1,27 @@
 print("\n----- part 1, places in the world -----")
+# think of at least 5 places in the world that you'd like to visit
+# 1) store the locations in a list
+# 2) use len() to print a message displaying how many places you'd like to visit
+# 3) print your list in the original order
+# 4) use sorted() to print your list in alphabetical order without modifying the actual list
+# 5) show that your original list is still in its original order by printing it
+# 6) use sorted() to print your list in reverse alphabetical order without changing the original list
+# 7) show that your list is still in its original order by printing it again
+# 8) use reverse() to change the order of your list, print the list to show that the order has changed
+# 9) use reverse() to change the order of your list again, print the list to show it's back to its original order
+# 10) use sort() to change your list so it's stored in alphabetical order, print the list
+# 11) use sort to change your list so it's in reverse alphabetical order, print your list
 
 
 
 
 print("\n\n----- part 2, swap -----")
+# input a string, convert to a list of items
+# swap the first and last item
 
 
 
 
 print("\n\n----- part 3, duplicates -----")
+# input a string, determine if any words repeat
+# hint: convert to list, then use set() and len()
