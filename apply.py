3698064038a93cd99920eab0a38c80e8d9487ae7
heyscottn <heyscottn@gmail.com>
2024-01-19 12:46:28 -0600

    edits

--- apply.py
+++ apply.py
@@ -2,26 +2,46 @@ print("\n----- part 1, places in the world -----")
 # think of at least 5 places in the world that you'd like to visit
 # 1) store the locations in a list
 # 2) use len() to print a message displaying how many places you'd like to visit
+print("2) How many:")
+
 # 3) print your list in the original order
+print("3) Here they are:")
+
 # 4) use sorted() to print your list in alphabetical order without modifying the actual list
+print("4) Using sorted():")
+
 # 5) show that your original list is still in its original order by printing it
+print("5) No change:")
+
 # 6) use sorted() to print your list in reverse alphabetical order without changing the original list
+print("6) sorted() with reverse option: ")
+
 # 7) show that your list is still in its original order by printing it again
+print("7) Still no change:")
+
 # 8) use reverse() to change the order of your list, print the list to show that the order has changed
+print("8) using .reverse():")
+
 # 9) use reverse() to change the order of your list again, print the list to show it's back to its original order
+print("9) .reverse() again:")
+
 # 10) use sort() to change your list so it's stored in alphabetical order, print the list
+print("10) .sort():")
+
 # 11) use sort to change your list so it's in reverse alphabetical order, print your list
+print("11) .sort() with reverse option:")
 
 
 
 
-print("\n\n----- part 2, swap -----")
-# input a string, convert to a list of items
-# swap the first and last item
 
-
-
-
-print("\n\n----- part 3, duplicates -----")
+print("\n\n----- part 2, duplicates -----")
 # input a string, determine if any words repeat
 # hint: convert to list, then use set() and len()
+
+
+
+
+print("\n\n----- part 3, swap -----")
+# input a string, convert to a list of items
+# swap the first and last item
