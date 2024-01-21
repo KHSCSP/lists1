d822716c76b24c2efeb75d51301aad8081390050
heyscottn <heyscottn@gmail.com>
2024-01-21 10:47:15 -0600

    sn

--- try1.py
+++ try1.py
@@ -2,8 +2,33 @@ print("\n---- working with ints ----")
 nums = [12, 0, -5, 42, 99, 85, 13]
 # TODO experiment with len, min, max, sum
 # TODO experiment with append/insert, remove/del, index, count, sort/sorted, reverse
+print("the list:")
+print("length:")
+print("minimum:")
+print("maximum:")
+print("total:")
+print("using sorted():")
+print("using sorted() with reverse option:")
+print("using .sort():")
+print("using .reverse():")
 
 
+print("using .append():")
+print("using .insert():")
+print("using .remove():")
+print("using del:")
+print("using .index():")
+
+# .sort()
+# wrong nums = nums.sort()
+# wrong print(nums.sort())
+# right nums.sort()
+
+# sorted()
+# wrong nums.sorted()
+# wrong nums = nums.sorted()
+# right nums = sorted(nums)
+# right print(sorted(nums))
 
 
 
